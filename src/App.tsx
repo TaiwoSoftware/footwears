b507ea3b6589ed65e8b1ca02c5f71fe6c051c9b6
taiwosoftware <davcodes24@gmail.com>
2024-11-18 18:28:07 +0100

    done with installing all the necessary frameworks

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-center'>Hello world</h1>
+      
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export const Navbar:React.FC = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
